41bf5332baa93456b22da69f1fc2d0dd61b0129c
Anton Varlamkin <fr.varlamkin@mail.ru>
2020-10-13 23:06:22 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import api from './api/api'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [query, setQuery] = useState('')
+    const [weather, setWeather] = useState({})
+
+    const search = evt => {
+        if (evt.key === "Enter") {
+            fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+                .then(res => res.json())
+                .then(result => {
+                    setWeather(result);
+                    setQuery('')
+                    console.log(weather)
+                });
+        }
+    }
+    const dateBuilder = (d) => {
+        let months = ['января', 'февраля', 'марта',
+            'апреля', 'мая', 'июня',
+            'июля', 'августа', 'сентября',
+            'октября', 'ноября', 'декабря'];
+        let days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+        return `${day} ${date} ${month} ${year}`
+    }
+    return (
+        <div className={(typeof  weather.main != 'undefined')
+            ? (weather.main.temp > 16
+                ? 'app warm' : 'app')
+            : 'app'}>
+            <main>
+                <div className="search-box">
+                    <input type="text"
+                           className={'search-bar'}
+                           placeholder={'Введите город...'}
+                           onChange={e => setQuery(e.target.value)}
+                           value={query}
+                           onKeyPress={search}/>
+                </div>
+                {(typeof weather.main != 'undefined') ? (
+                    <div>
+                        <div className="location-box">
+                            <div className="location">{weather.name}, {weather.sys.country}</div>
+                            <div className="date">{dateBuilder(new Date())}</div>
+                        </div>
+                        <div className="weather-box">
+                            <div className="temp">
+                                {Math.round(weather.main.temp)}°C
+                            </div>
+                            <div className="weather">
+                                {weather.weather[0].main}
+                            </div>
+                        </div>
+                    </div>
+                ) : ('')}
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/api/api.js
@@ -0,0 +1,6 @@
+const api = {
+    key:'8f0c7c1f66133dfd041dffaac9ffd8a4',
+    base:'https://api.openweathermap.org/data/2.5/'
+}
+
+export default api;
